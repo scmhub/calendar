38a257600645fa19833de64f2c2fe41c539ffbf7
Philippe Chavanne <philippe.chavanne@gmail.com>
2021-05-25 15:52:27 +0200

    Calc

--- holiday_test.go
+++ holiday_test.go
@@ -22,10 +22,32 @@ func TestCopy(t *testing.T) {
 	assert.NotEmpty(h2.observance)
 }
 
+func TestCalc(t *testing.T) {
+	assert := assert.New(t)
+	h := &Holiday{
+		Name:  "A holiday",
+		Month: time.March,
+		Day:   17,
+		calc:  CalcDayOfMonth,
+	}
+	assert.Equal(time.Date(2020, 3, 17, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.Offset = 1
+	assert.Equal(time.Date(2020, 3, 18, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.Offset = -1
+	assert.Equal(time.Date(2020, 3, 16, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.Offset = -2
+	assert.Equal(time.Date(2020, 3, 15, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.SetObservance(nearestWorkday)
+	assert.Equal(time.Date(2020, 3, 16, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.Year = 2020
+	assert.Equal(time.Date(2020, 3, 16, 0, 0, 0, 0, Paris), h.Calc(2020, Paris))
+	h.Year = 2021
+	assert.Equal(time.Time{}, h.Calc(2020, Paris))
+}
+
 func TestCalcDayOfMonth(t *testing.T) {
 	assert := assert.New(t)
 	h := &Holiday{Month: time.December, Day: 25}
-	paris, _ := time.LoadLocation("Europe/Paris")
-	assert.Equal(time.Date(2020, 12, 25, 0, 0, 0, 0, paris), CalcDayOfMonth(h, 2020, paris))
-	assert.Equal(time.Date(2021, 12, 25, 0, 0, 0, 0, paris), CalcDayOfMonth(h, 2021, paris))
+	assert.Equal(time.Date(2020, 12, 25, 0, 0, 0, 0, Paris), CalcDayOfMonth(h, 2020, Paris))
+	assert.Equal(time.Date(2021, 12, 25, 0, 0, 0, 0, Paris), CalcDayOfMonth(h, 2021, Paris))
 }
