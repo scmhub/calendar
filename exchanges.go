f32c44930027ff0a016d40ea010f0f889988c134
Philippe Chavanne <philippe.chavanne@gmail.com>
2021-05-27 07:37:18 +0200

    years...

--- exchanges.go
+++ exchanges.go
@@ -5,29 +5,29 @@ import "time"
 // America
 
 // New York Stock Exchange
-func NYSE(start, end int) *Calendar {
-	c := NewCalendar("New York Stock Exchange", NewYork, start, end)
+func NYSE(years ...int) *Calendar {
+	c := NewCalendar("New York Stock Exchange", NewYork, years...)
 	//TODO: add holidays
 	return c
 }
 
 // NASDAQ
-func NASDAQ(start, end int) *Calendar {
-	c := NewCalendar("NASDAQ", NewYork, start, end)
+func NASDAQ(years ...int) *Calendar {
+	c := NewCalendar("NASDAQ", NewYork, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Chicago Board Options Exchange
-func CBOE(start, end int) *Calendar {
-	c := NewCalendar("Chicago Board Options Exchange", Chicago, start, end)
+func CBOE(years ...int) *Calendar {
+	c := NewCalendar("Chicago Board Options Exchange", Chicago, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Cboe Futures Exchange
-func CFE(start, end int) *Calendar {
-	c := NewCalendar("Cboe Futures Exchange", Chicago, start, end)
+func CFE(years ...int) *Calendar {
+	c := NewCalendar("Cboe Futures Exchange", Chicago, years...)
 	//TODO: add holidays
 	return c
 }
@@ -35,8 +35,8 @@ func CFE(start, end int) *Calendar {
 // Europe
 
 // London Stock Exchange
-func LSE(start, end int) *Calendar {
-	c := NewCalendar("London Stock Exchange", London, start, end)
+func LSE(years ...int) *Calendar {
+	c := NewCalendar("London Stock Exchange", London, years...)
 	c.addHoliday(NewYear.Copy("New Year's Day").SetObservance(nextMonday))
 	c.addHoliday(GoodFriday.Copy("Good Friday"))
 	c.addHoliday(EasterMonday.Copy("Easter Monday"))
@@ -50,8 +50,8 @@ func LSE(start, end int) *Calendar {
 }
 
 // euronext
-func euronext(name string, loc *time.Location, start, end int) *Calendar {
-	c := NewCalendar(name, loc, start, end)
+func euronext(name string, loc *time.Location, years ...int) *Calendar {
+	c := NewCalendar(name, loc, years...)
 	c.addHoliday(NewYear.Copy("New Year's Day"))
 	c.addHoliday(GoodFriday.Copy("Good Friday"))
 	c.addHoliday(EasterMonday.Copy("Easter Monday"))
@@ -62,65 +62,65 @@ func euronext(name string, loc *time.Location, start, end int) *Calendar {
 }
 
 // Euronext Amsterdam
-func EuronextAmsterdam(start, end int) *Calendar {
-	return euronext("Euronext Amsterdam", Amsterdam, start, end)
+func EuronextAmsterdam(years ...int) *Calendar {
+	return euronext("Euronext Amsterdam", Amsterdam, years...)
 }
 
 // Euronext Brussels
-func EuronextBrussels(start, end int) *Calendar {
-	return euronext("Euronext Brussels", Brussels, start, end)
+func EuronextBrussels(years ...int) *Calendar {
+	return euronext("Euronext Brussels", Brussels, years...)
 }
 
 // Euronext Lisbon
-func EuronextLisbon(start, end int) *Calendar {
-	return euronext("Euronext Lisbon", Lisbon, start, end)
+func EuronextLisbon(years ...int) *Calendar {
+	return euronext("Euronext Lisbon", Lisbon, years...)
 }
 
 // Euronext Paris
-func EuronextParis(start, end int) *Calendar {
-	return euronext("Euronext Paris", Paris, start, end)
+func EuronextParis(years ...int) *Calendar {
+	return euronext("Euronext Paris", Paris, years...)
 }
 
 // Asia
 
 // Hong Kong Exchanges
-func HKE(start, end int) *Calendar {
-	c := NewCalendar("Hong Kong Exchanges", HongKong, start, end)
+func HKE(years ...int) *Calendar {
+	c := NewCalendar("Hong Kong Exchanges", HongKong, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Shenzhen Stock Exchange
-func ShenzhenSE(start, end int) *Calendar {
-	c := NewCalendar("Shenzhen Stock Exchange", Shenzhen, start, end)
+func ShenzhenSE(years ...int) *Calendar {
+	c := NewCalendar("Shenzhen Stock Exchange", Shenzhen, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Shanghai Stock Exchange
-func ShanghaiSE(start, end int) *Calendar {
-	c := NewCalendar("Shanghai Stock Exchange", Shanghai, start, end)
+func ShanghaiSE(years ...int) *Calendar {
+	c := NewCalendar("Shanghai Stock Exchange", Shanghai, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Korea Exchange
-func KRX(start, end int) *Calendar {
-	c := NewCalendar("Korea Exchange", Seoul, start, end)
+func KRX(years ...int) *Calendar {
+	c := NewCalendar("Korea Exchange", Seoul, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Japan Exchange Group
-func JEG(start, end int) *Calendar {
-	c := NewCalendar("Japan Exchange Group", Tokyo, start, end)
+func JEG(years ...int) *Calendar {
+	c := NewCalendar("Japan Exchange Group", Tokyo, years...)
 	//TODO: add holidays
 	return c
 }
 
 // Australian Securities Exchange
-func ASE(start, end int) *Calendar {
-	c := NewCalendar("Australian Securities Exchange", Sydney, start, end)
+func ASE(years ...int) *Calendar {
+	c := NewCalendar("Australian Securities Exchange", Sydney, years...)
 	//TODO: add holidays
 	return c
 }
